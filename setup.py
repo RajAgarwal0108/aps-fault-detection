6acb9949c6a7690ef8ae2ac46b9513f8c77ef592
raj agaral <rajagarwal0108@gmail.com>
2023-01-17 18:58:37 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+
+from typing import List
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+HYPHEN_E_DOT = '-e.'
+
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [name.replace("/n","") for name in requirement_list]
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="Raj_Agarwal",
+    author_email="rajagarwal0108@gmail.com",
+    packages=find_packages(),
+    install_requires = get_requirements(),
+)
