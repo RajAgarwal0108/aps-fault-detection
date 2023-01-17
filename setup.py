c9842dc26531cd82d5f26986b0919625c100c750
raj agaral <rajagarwal0108@gmail.com>
2023-01-17 19:59:50 +0530

    file_names_created

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import find_packages,setup
 from typing import List
 
 REQUIREMENT_FILE_NAME = "requirements.txt"
-HYPHEN_E_DOT = '-e.'
+HYPHEN_E_DOT = "-e ."
 
 def get_requirements()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
-    requirement_list = [name.replace("/n","") for name in requirement_list]
+    requirement_list = [name.replace("\n","") for name in requirement_list]
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
+    
     return requirement_list
 
 setup(
@@ -19,5 +20,5 @@ setup(
     author="Raj_Agarwal",
     author_email="rajagarwal0108@gmail.com",
     packages=find_packages(),
-    install_requires = get_requirements(),
+    install_requires = get_requirements()
 )
